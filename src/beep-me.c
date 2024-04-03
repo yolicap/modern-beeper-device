073a656ed01ea1a56e9d8de472592fdb137d54ba
yolicap <zeusapolanco@gmail.com>
2024-04-03 14:48:46 -0400

    HTTP server base. Untested

--- src/beep-me.c
+++ src/beep-me.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
+#include "http/http_server.c"
 
-void app_main(void)
-{
+void app_main(void) {
+    app_init();
+}
 
+void app_init() {
+    nvs_flash_init();
+    system_init();
+
+    // When beeper-mode enabled:
+    wifi_init();
+    xTaskCreate(&http_server, "http_server", 2048, NULL, 5, NULL);
 }
 
 void display_menu(){
